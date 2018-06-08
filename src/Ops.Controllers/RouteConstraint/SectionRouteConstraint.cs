474ddfa5bcb6bee72ad94b6c6f1b99cf8f704586
Dan <danielwilcox@mcldaz.org>
2018-06-08 14:24:17 -0700

    Add section routing

---
+++ src/Ops.Controllers/RouteConstraint/SectionRouteConstraint.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Ocuda.Ops.Controllers.RouteConstraint
+{
+    public class SectionRouteConstraint : IRouteConstraint
+    {
+        public SectionRouteConstraint()
+        {
+        }
+
+        public bool Match(HttpContext httpContext,
+            IRouter route,
+            string routeKey,
+            RouteValueDictionary values,
+            RouteDirection routeDirection)
+        {
+            var site = (string)values[routeKey];
+            if (site == "HumanResources" || site == "Communications")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
