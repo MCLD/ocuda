0a60ebc9ada0c05892a42ba4e4cbc4290842e93e
Dan Wilcox <iafb@users.noreply.github.com>
2018-06-28 12:14:51 -0700

    Add unit testing (#22)
    - Add unit testing

---
+++ test/Ops/Ops.Service.Test/SectionService_Tests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using Moq;
+using Ocuda.Ops.Models;
+using Ocuda.Ops.Service;
+using Ocuda.Ops.Service.Interfaces.Ops;
+using Xunit;
+
+namespace Ocuda.test.Ops.Service.Test
+{
+    public class SectionService_Tests
+    {
+        [Fact]
+        public async Task EditAsync_SetCorrectFields()
+        {
+            var currentSection = new Section()
+            {
+                Id = 5,
+                CreatedAt = DateTime.Parse("2018-06-24"),
+                CreatedBy = 1,
+                Name = "Test",
+                Icon = "fa-test",
+                Path = "Test"
+            };
+            var editedSection = new Section()
+            {
+                Id = 5,
+                CreatedAt = DateTime.Parse("2016-11-22"),
+                CreatedBy = 20,
+                Name = "New Name",
+                Icon = "fa-test2",
+                Path = "TestPath"
+            };
+            var expectedSection = new Section()
+            {
+                Id = 5,
+                CreatedAt = DateTime.Parse("2018-06-24"),
+                CreatedBy = 1,
+                Name = "New Name",
+                Icon = "fa-test2",
+                Path = "TestPath"
+            };
+
+            var sectionRepository = new Mock<ISectionRepository>();
+            sectionRepository.Setup(_ => _.FindAsync(5)).Returns(Task.FromResult(currentSection));
+
+            var service = new SectionService(sectionRepository.Object);
+
+            editedSection = await service.EditAsync(editedSection);
+
+            Assert.Equal(expectedSection.Id, editedSection.Id);
+            Assert.Equal(expectedSection.CreatedAt, editedSection.CreatedAt);
+            Assert.Equal(expectedSection.CreatedBy, editedSection.CreatedBy);
+            Assert.Equal(expectedSection.Name, editedSection.Name);
+            Assert.Equal(expectedSection.Icon, editedSection.Icon);
+            Assert.Equal(expectedSection.Path, editedSection.Path);
+        }
+    }
+}
