a88ab81e97cffffe0b4155d51d46c6535e362842
Dan <danielwilcox@mcldaz.org>
2018-07-16 11:51:07 -0700

    Add PRG model state storage

---
+++ src/Utility/Helpers/ModelStateHelpers.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Routing;
+using Newtonsoft.Json;
+using Ocuda.Utility.Models;
+
+namespace Ocuda.Utility.Helpers
+{
+    public static class ModelStateHelpers
+    {
+        public static string SerializeModelState(ModelStateDictionary modelState)
+        {
+            var itemList = modelState
+                .Select(_ => new ModelStateItem
+                {
+                    Key = _.Key,
+                    AttemptedValue = _.Value.AttemptedValue,
+                    RawValue = _.Value.RawValue,
+                    ErrorMessages = _.Value.Errors.Select(e => e.ErrorMessage).ToList(),
+                })
+                .ToList();
+
+            var storage = new ModelStateStorage()
+            {
+                ModelStateItems = itemList,
+                Time = DateTimeOffset.Now.ToUnixTimeSeconds()
+            };
+
+            return JsonConvert.SerializeObject(storage);
+        }
+
+        public static (ModelStateDictionary modelState, long time) DeserializeModelState(
+            string modelStateStorage)
+        {
+            var storage = JsonConvert.DeserializeObject<ModelStateStorage>(modelStateStorage);
+            var modelState = new ModelStateDictionary();
+
+            foreach (var item in storage.ModelStateItems)
+            {
+                modelState.SetModelValue(item.Key, item.RawValue, item.AttemptedValue);
+                foreach (var error in item.ErrorMessages)
+                {
+                    modelState.AddModelError(item.Key, error);
+                }
+            }
+            return (modelState, storage.Time);
+        }
+
+        public static string GetModelStateKey(RouteValueDictionary routeValues)
+        {
+            var routeString = string.Join("-", routeValues.Values).ToLower();
+            return $"ModelState_{routeString}";
+        }
+    }
+
+}
---
+++ src/Utility/Models/ModelStateStorage.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Ocuda.Utility.Models
+{
+    public class ModelStateStorage
+    {
+        public List<ModelStateItem> ModelStateItems { get; set; }
+        public long Time { get; set; }
+    }
+}
