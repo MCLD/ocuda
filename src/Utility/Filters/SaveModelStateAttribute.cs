a125592040f0993e2a07b6db82b865d927c604d2
Dan Wilcox <iafb@users.noreply.github.com>
2018-07-16 16:12:55 -0700

    Add PRG model state storage (#40)
    - Add post-redirect-get ModelState storage

---
+++ src/Utility/Filters/SaveModelStateAttribute.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Ocuda.Utility.Helpers;
+
+namespace Ocuda.Utility.Filters
+{
+    public class SaveModelStateAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            //Only export when ModelState is not valid
+            if (!context.ModelState.IsValid)
+            {
+                //Export if we are redirecting
+                if (context.Result is RedirectResult
+                    || context.Result is RedirectToRouteResult
+                    || context.Result is RedirectToActionResult)
+                {
+                    var controller = context.Controller as Controller;
+                    if (controller != null && context.ModelState != null)
+                    {
+                        var key = ModelStateHelpers.GetModelStateKey(context.RouteData.Values);
+                        controller.TempData[key] = ModelStateHelpers
+                            .SerializeModelState(context.ModelState);
+                    }
+                }
+            }
+
+            base.OnActionExecuted(context);
+        }
+    }
+}
