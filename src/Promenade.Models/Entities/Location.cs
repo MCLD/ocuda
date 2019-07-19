0b70e13c958977636deb3d7527e1e0dc3c8b110e
Madison Boman <madisonboman@gmail.com>
2019-07-19 12:28:42 -0700

    Fix Build

--- src/Promenade.Models/Entities/Location.cs
+++ src/Promenade.Models/Entities/Location.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Web.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Ocuda.Promenade.Models.Entities
 {
