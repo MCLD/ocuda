3312cbb35b655df3298098714c1e119d3842c9a5
Madison Boman <madisonboman@gmail.com>
2019-07-23 08:04:02 -0700

    Removed the unused

--- src/Promenade.Service/Interfaces/Repositories/IGroupRepository.cs
+++ src/Promenade.Service/Interfaces/Repositories/IGroupRepository.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Ocuda.Ops.Service.Interfaces.Ops.Repositories;
-using Ocuda.Promenade.Models.Entities;
+﻿using Ocuda.Promenade.Models.Entities;
 
 namespace Ocuda.Promenade.Service.Interfaces.Repositories
 {
--- src/Promenade.Service/Interfaces/Repositories/ILocationHoursRepository.cs
+++ src/Promenade.Service/Interfaces/Repositories/ILocationHoursRepository.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Ocuda.Ops.Service.Interfaces.Ops.Repositories;
 using Ocuda.Promenade.Models.Entities;
 
 namespace Ocuda.Promenade.Service.Interfaces.Repositories
