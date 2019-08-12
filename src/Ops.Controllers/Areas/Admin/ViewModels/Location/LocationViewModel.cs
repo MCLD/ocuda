3c350d0497eeb18d23e35bb70df6136e7a84c136
Madison Boman <madisonboman@gmail.com>
2019-08-12 11:03:24 -0700

    stuff

--- src/Ops.Controllers/Areas/Admin/ViewModels/Location/LocationViewModel.cs
+++ src/Ops.Controllers/Areas/Admin/ViewModels/Location/LocationViewModel.cs
@@ -10,12 +10,15 @@ namespace Ocuda.Ops.Controllers.Areas.Admin.ViewModels.Location
 
         public Promenade.Models.Entities.Location Location { get; set; }
         public List<Promenade.Models.Entities.LocationFeature> LocationFeatures { get; set; }
+        public List<Promenade.Models.Entities.LocationFeature> NonLocationFeatures { get; set; }
         public List<Promenade.Models.Entities.Feature> Features { get; set; }
+        public List<Promenade.Models.Entities.Feature> NonFeatures { get; set; }
         public Promenade.Models.Entities.Feature Feature { get; set; }
         public List<Promenade.Models.Entities.LocationGroup> LocationGroups { get; set; }
+        public List<Promenade.Models.Entities.LocationGroup> NonLocationGroups { get; set; }
         public List<Promenade.Models.Entities.Group> Groups { get; set; }
+        public List<Promenade.Models.Entities.Group> NonGroups { get; set; }
         public Promenade.Models.Entities.Group Group { get; set; }
-        public bool IsSavedLocation { get; set; }
         public string Action { get; set; }
         public PaginateModel PaginateModel { get; set; }
     }
