f7ce6a99cb4337392d7dd6ba271cee958ca80fca
Madison Boman <madisonboman@gmail.com>
2019-10-11 10:18:02 -0700

    Build fix?

--- src/polaris-api-csharp/Properties/AssemblyInfo.cs
+++ src/polaris-api-csharp/Properties/AssemblyInfo.cs
@@ -4,13 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-[assembly: AssemblyTitle("Polaris API Library")]
 [assembly: AssemblyDescription("Polaris API helper library")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Central Library Consortium, Michael Fields")]
-[assembly: AssemblyProduct("Polaris API Library")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -23,17 +17,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("d8cf2eee-b92e-4769-8ad7-4d8fe226bb79")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-//[assembly: AssemblyVersion("2.0.10")]
-[assembly: AssemblyInformationalVersion("2.1.3-beta")]
