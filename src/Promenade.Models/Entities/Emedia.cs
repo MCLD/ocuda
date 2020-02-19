74ec63a2650e4203c47515acce0305b4f6ee8533
Dan Wilcox <danielwilcox@mcldaz.org>
2020-02-19 16:55:51 -0700

    Change emedia group to be mandatory

--- src/Promenade.Models/Entities/Emedia.cs
+++ src/Promenade.Models/Entities/Emedia.cs
@@ -18,7 +18,7 @@ namespace Ocuda.Promenade.Models.Entities
         [Required]
         public string Stub { get; set; }
 
-        public int? GroupId { get; set; }
+        public int GroupId { get; set; }
         public EmediaGroup Group { get; set; }
 
         [MaxLength(255)]
