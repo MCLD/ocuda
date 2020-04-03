dbc0b3455ab8f6ee1acd254f1829f10b5f11477c
Dan Wilcox <danielwilcox@mcldaz.org>
2020-04-03 11:14:58 -0700

    Change nav menu to be generated from a file
    Change navigation filter to generate the navigation menu from a file instead of configuration

--- src/Ops.Controllers/Filters/NavigationFilter.cs
+++ src/Ops.Controllers/Filters/NavigationFilter.cs
@@ -1,35 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Caching.Distributed;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using Ocuda.Ops.Models;
-using Ocuda.Utility.Keys;
+using Ocuda.Utility.Services.Interfaces;
 
 namespace Ocuda.Ops.Controllers.Filters
 {
     public class NavigationFilterAttribute : Attribute, IAsyncActionFilter
     {
+        private const string NavMenuFileName = "NavMenu.json";
+
         private readonly ILogger<NavigationFilterAttribute> _logger;
-        private readonly IConfiguration _config;
         private readonly IDistributedCache _cache;
+        private readonly IPathResolverService _pathResolverService;
 
         public NavigationFilterAttribute(ILogger<NavigationFilterAttribute> logger,
-           IConfiguration config,
-           IDistributedCache cache)
+           IDistributedCache cache,
+           IPathResolverService pathResolverService)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _cache = cache ?? throw new ArgumentNullException(nameof(cache));
-            _config = config ?? throw new ArgumentNullException(nameof(config));
+            _pathResolverService = pathResolverService
+                ?? throw new ArgumentNullException(nameof(pathResolverService));
         }
 
-        public async Task OnActionExecutionAsync(ActionExecutingContext context, 
+        public async Task OnActionExecutionAsync(ActionExecutingContext context,
             ActionExecutionDelegate next)
         {
-            context.HttpContext.Items[ItemKey.NavColumn] = 
-                _config.GetSection(Configuration.OpsNavColumn).Get<List<NavigationRow>>();
+            var navMenuJsonFile = _pathResolverService.GetPrivateContentFilePath(NavMenuFileName);
+            using (StreamReader file = File.OpenText(navMenuJsonFile))
+            {
+                var serializer = new JsonSerializer();
+                context.HttpContext.Items[ItemKey.NavColumn] = (List<NavigationRow>)serializer
+                    .Deserialize(file, typeof(List<NavigationRow>));
+            }
 
             await next();
         }
