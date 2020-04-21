a9aa085937905376f0446dab9acf606af9f5b2fa
Harald Nagel <haraldnagel@mcldaz.org>
2020-04-21 13:09:40 -0700

    Add same-day scheduling with a window

---
+++ src/Utility/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ocuda.Utility.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime RoundUp(this DateTime dateTime, TimeSpan roundTo)
+        {
+            return new DateTime((dateTime.Ticks + roundTo.Ticks - 1)
+                / roundTo.Ticks * roundTo.Ticks, dateTime.Kind);
+        }
+    }
+}
