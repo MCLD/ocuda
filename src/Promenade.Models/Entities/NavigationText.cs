c50577540ca17d569f67ca3bb9f821154c5a74b9
Harald Nagel <haraldnagel@mcldaz.org>
2021-07-20 15:26:01 -0700

    Remove nav aria field; allow l10n of nav links
    - Remove now-unused aria field from navigation database
    - Move navigation links to localized table so they can differ per
      language

--- src/Promenade.Models/Entities/NavigationText.cs
+++ src/Promenade.Models/Entities/NavigationText.cs
@@ -5,14 +5,14 @@ namespace Ocuda.Promenade.Models.Entities
     public class NavigationText
     {
         public int Id { get; set; }
-        public int LanguageId { get; set; }
-
-        [MaxLength(255)]
-        public string AriaLabel { get; set; }
-
         [MaxLength(255)]
         public string Label { get; set; }
 
+        public int LanguageId { get; set; }
+
+        [MaxLength(255)]
+
+        public string Link { get; set; }
         [MaxLength(255)]
         public string Title { get; set; }
     }
