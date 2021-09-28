93cde5e7ff6e8e414d0ed988d49545e941d32858
Harald Nagel <haraldnagel@mcldaz.org>
2021-09-28 12:57:28 -0700

    Fix error in Ops.Data.Test

---
+++ test/Ops/Ops.Data.Test/ContextFixture.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ops.Data.Test
+{
+    public class ContextFixture : IDisposable
+    {
+        public ContextFixture()
+        {
+            var opsPromOptions
+                = new DbContextOptionsBuilder<Ocuda.Ops.DataProvider.SqlServer.Promenade.Context>()
+                .UseInMemoryDatabase(databaseName: "Promenade")
+                .Options;
+
+            var promPromOptions
+                = new DbContextOptionsBuilder<Ocuda.Promenade.DataProvider.SqlServer.Promenade.Context>()
+                .UseInMemoryDatabase(databaseName: "Promenade")
+                .Options;
+
+            OpsProm = new Ocuda.Ops.DataProvider.SqlServer.Promenade.Context(opsPromOptions);
+            PromProm = new Ocuda.Promenade.DataProvider.SqlServer.Promenade.Context(promPromOptions);
+
+            OpsPromDbSets = OpsProm.GetType()
+               .GetProperties()
+               .Where(_ => _.PropertyType.IsGenericType
+               && typeof(DbSet<>).IsAssignableFrom(_.PropertyType.GetGenericTypeDefinition()))
+               .Select(_ => _.Name);
+
+            PromPromDbSets = PromProm.GetType()
+               .GetProperties()
+               .Where(_ => _.PropertyType.IsGenericType
+                   && typeof(DbSet<>).IsAssignableFrom(_.PropertyType.GetGenericTypeDefinition()))
+               .Select(_ => _.Name);
+        }
+
+        public Ocuda.Ops.DataProvider.SqlServer.Promenade.Context OpsProm { get; set; }
+        public IEnumerable<string> OpsPromDbSets { get; set; }
+        public Ocuda.Promenade.DataProvider.SqlServer.Promenade.Context PromProm { get; set; }
+        public IEnumerable<string> PromPromDbSets { get; set; }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                OpsProm?.Dispose();
+                OpsProm = null;
+
+                PromProm?.Dispose();
+                PromProm = null;
+            }
+        }
+    }
+}
--- test/Ops/Ops.Data.Test/PromenadeMatchTest.cs
+++ test/Ops/Ops.Data.Test/PromenadeMatchTest.cs
@@ -1,48 +1,23 @@
 using System.Linq;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
 
 namespace Ops.Data.Test
 {
-    [TestClass]
-    public class PromenadeMatch
+    public class PromenadeMatch : IClassFixture<ContextFixture>
     {
-        [TestMethod]
-        public void VerifyMatch()
+        private readonly ContextFixture _fixture;
+
+        public PromenadeMatch(ContextFixture fixture)
         {
-            var opsPromOptions
-                = new DbContextOptionsBuilder<Ocuda.Ops.DataProvider.SqlServer.Promenade.Context>()
-                .UseInMemoryDatabase(databaseName: "Promenade")
-                .Options;
-
-            var promPromOptions
-                = new DbContextOptionsBuilder<Ocuda.Promenade.DataProvider.SqlServer.Promenade.Context>()
-                .UseInMemoryDatabase(databaseName: "Promenade")
-                .Options;
-
-            using var opsProm = new Ocuda.Ops.DataProvider.SqlServer.Promenade.Context(opsPromOptions);
-            using var promProm = new Ocuda.Promenade.DataProvider.SqlServer.Promenade.Context(promPromOptions);
-            var opsPromDbSets = opsProm.GetType()
-                .GetProperties()
-                .Where(_ => _.PropertyType.IsGenericType
-                && typeof(DbSet<>).IsAssignableFrom(_.PropertyType.GetGenericTypeDefinition()))
-                .Select(_ => _.Name);
-
-            var promPromDbSets = promProm.GetType()
-                .GetProperties()
-                .Where(_ => _.PropertyType.IsGenericType
-                    && typeof(DbSet<>).IsAssignableFrom(_.PropertyType.GetGenericTypeDefinition()))
-                .Select(_ => _.Name);
-
-            foreach (var dbSetName in opsPromDbSets.Except(promPromDbSets))
-            {
-                Assert.Fail("DbSet present in Ocuda.Ops Promenade Context but missing from Ocuda.Promenade Promenade Context: {0}", dbSetName);
-            }
-
-            foreach (var dbSetName in promPromDbSets.Except(opsPromDbSets))
-            {
-                Assert.Fail("DbSet present in Ocuda.Promenade Promenade Context but missing from Ocuda.Ops Promenade Context: {0}", dbSetName);
-            }
+            _fixture = fixture;
         }
+
+        [Fact]
+        public void OpsPromMissingPromProm()
+            => Assert.Empty(_fixture.OpsPromDbSets.Except(_fixture.PromPromDbSets));
+
+        [Fact]
+        public void PromPromMissingOpsProm()
+            => Assert.Empty(_fixture.PromPromDbSets.Except(_fixture.OpsPromDbSets));
     }
 }
