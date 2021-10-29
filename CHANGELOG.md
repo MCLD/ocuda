99917a0d882475d00201aba136760cd417ef5dd2
Dan Wilcox <danielwilcox@mcldaz.org>
2021-10-29 15:10:10 -0700

    Add migration

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -30,6 +30,7 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/).
 - Podcast functionality to promenade
 - Add layout pages
 - Application permission handling and administration
+- Emedia management
 
 ### Changed
 - Default to only logging Warnings for Microsoft and System namespaces
---
+++ src/Ops.DataProvider.SqlServer.Promenade/Migrations/20211029220453_prom_v.0.0.152.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ocuda.Ops.DataProvider.SqlServer.Promenade.Migrations
+{
+    public partial class prom_v00152 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Stub",
+                table: "Emedia");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Stub",
+                table: "Emedia",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
