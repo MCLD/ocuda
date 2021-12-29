44f61a2e653c5925a031997b7d0d0ba16edaf36d
Harald Nagel <haraldnagel@mcldaz.org>
2021-12-29 13:24:29 -0700

    Improve search and back button page logic

--- src/Ops.Controllers/Areas/Incident/ViewModel/HistoricalDetailsViewModel.cs
+++ src/Ops.Controllers/Areas/Incident/ViewModel/HistoricalDetailsViewModel.cs
@@ -4,8 +4,18 @@ namespace Ocuda.Ops.Controllers.Areas.Incident.ViewModel
 {
     public class HistoricalDetailsViewModel
     {
+        public string BackAction
+        {
+            get
+            {
+                return string.IsNullOrEmpty(SearchText)
+                     ? nameof(HistoricalController.Index)
+                     : nameof(HistoricalController.Search);
+            }
+        }
+
         public HistoricalIncident HistoricalIncident { get; set; }
-        public int? Page { get; set; }
+        public int Page { get; set; }
         public string SearchText { get; set; }
     }
 }
