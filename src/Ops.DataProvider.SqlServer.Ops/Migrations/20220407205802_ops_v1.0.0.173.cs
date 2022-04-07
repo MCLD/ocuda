c563e11f9f218abd99aa90a64a222748550c1a2d
Harald Nagel <haraldnagel@mcldaz.org>
2022-04-07 14:00:34 -0700

    Add migration; increment revision

---
+++ src/Ops.DataProvider.SqlServer.Ops/Migrations/20220407205802_ops_v1.0.0.173.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ocuda.Ops.DataProvider.SqlServer.Ops.Migrations
+{
+    public partial class ops_v100173 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PermissionGroupProductManager",
+                columns: table => new
+                {
+                    PermissionGroupId = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PermissionGroupProductManager", x => new { x.PermissionGroupId, x.ProductId });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PermissionGroupProductManager");
+        }
+    }
+}
