35a965cdf619cfa822ed899f6e548ec33de13ee1
Harald Nagel <dev@hpn.is>
2023-01-20 14:50:02 -0700

    Add migration for previous merge

---
+++ src/Ops.DataProvider.SqlServer.Ops/Migrations/20230120212854_ops_v1.0.0.222.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ocuda.Ops.DataProvider.SqlServer.Ops.Migrations
+{
+    /// <inheritdoc />
+    public partial class opsv100222 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Department",
+                table: "Users",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Mobile",
+                table: "Users",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Department",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Mobile",
+                table: "Users");
+        }
+    }
+}
