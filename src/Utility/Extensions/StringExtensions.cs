27adee2d71e06847fe6b90ddaa3f98e30ba31673
Harald Nagel <dev@hpn.is>
2023-01-20 17:15:14 -0700

    Fix LDAP update

---
+++ src/Utility/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Ocuda.Utility.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string TruncateTo(this string value, int length) =>
+            value?[0..Math.Min(value.Length, length)];
+    }
+}
