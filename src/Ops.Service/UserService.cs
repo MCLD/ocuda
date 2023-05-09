d83e8f4c8f4897061f81b3fb5bfbfef3fe327f4d
Harald Nagel <dev@hpn.is>
2023-05-09 16:33:54 -0700

    Improve product management

--- src/Ops.Service/UserService.cs
+++ src/Ops.Service/UserService.cs
@@ -19,7 +19,6 @@ using Ocuda.Utility.Keys;
 using Ocuda.Utility.Models;
 using Ocuda.Utility.Services.Interfaces;
 using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.Formats;
 
 namespace Ocuda.Ops.Service
 {
@@ -341,12 +340,13 @@ namespace Ocuda.Ops.Service
             try
             {
                 profilePicture = Convert.FromBase64String(profilePictureBase64);
-
-                var imageInfo = Image.Identify(profilePicture, out IImageFormat imageFormat);
+                var imageInfo = Image.Identify(profilePicture);
                 if (imageInfo.Height != imageInfo.Width)
                 {
                     throw new OcudaException("Profile picture must be square.");
                 }
+
+                var imageFormat = imageInfo.Metadata.DecodedImageFormat;
 
                 var validFileType = false;
                 foreach (var validExtension in ProfilePictureValidTypes)
