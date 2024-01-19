c078e7e68f08940b34177bd6a6425cc120791eaf
Kenny Heagle <kennyheagle@gmail.com>
2024-01-19 13:21:27 -0700

    Add migration files

---
+++ src/Ops.DataProvider.SqlServer.Promenade/Migrations/20240118201323_add_attributes_to_schedule_request_subject.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ocuda.Ops.DataProvider.SqlServer.Promenade.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_attributes_to_schedule_request_subject : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "ScheduleRequestSubject",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrderBy",
+                table: "ScheduleRequestSubject",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.Sql("UPDATE ScheduleRequestSubject SET IsActive=1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "ScheduleRequestSubject");
+
+            migrationBuilder.DropColumn(
+                name: "OrderBy",
+                table: "ScheduleRequestSubject");
+        }
+    }
+}
