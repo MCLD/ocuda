2adb05e1ddb19e524940673371acffa3e71c9dcb
Adam Abrams-Flohr <AdamAbrams-Flohr@mcldaz.org>
2025-04-18 14:35:42 -0700

    Remove [Table(“”)] annotations from BooksByMail entities

--- src/Ops.Models/Entities/BooksByMailComment.cs
+++ src/Ops.Models/Entities/BooksByMailComment.cs
@@ -5,7 +5,6 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Ocuda.Ops.Models.Entities
 {
-    [Table("BooksByMailComments")]
     public class BooksByMailComment
     {
         public int Id { get; set; }
--- src/Ops.Models/Entities/BooksByMailCustomer.cs
+++ src/Ops.Models/Entities/BooksByMailCustomer.cs
@@ -3,7 +3,6 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Ocuda.Ops.Models.Entities
 {
-    [Table("BooksByMailCustomers")]
     public class BooksByMailCustomer
     {
         public int Id { get; set; }
