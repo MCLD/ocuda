998b6277b32cb50be5aa6d5fbc5dff9b0b2fe3d4
Harald Nagel <dev@hpn.is>
2025-12-30 10:44:36 -0700

    Add Ocuda.Models project for API-based models

---
+++ src/Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ocuda.Models
+{
+    public class Customer
+    {
+        public IEnumerable<CustomerAddress> Addresses { get; set; }
+        public DateTime? AddressVerificationDate { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public string BlockingNotes { get; set; }
+        public int CustomerCodeId { get; set; }
+        public string CustomerIdNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime? ExpirationDate { get; set; }
+        public int Id { get; set; }
+        public bool IsBlocked { get; set; }
+        public string NameFirst { get; set; }
+        public string NameLast { get; set; }
+        public string Notes { get; set; }
+        public string UserDefinedField1 { get; set; }
+        public string UserDefinedField2 { get; set; }
+        public string UserDefinedField3 { get; set; }
+        public string UserDefinedField4 { get; set; }
+        public string UserDefinedField5 { get; set; }
+    }
+}
---
+++ src/Models/CustomerAddress.cs
@@ -0,0 +1,18 @@
+﻿namespace Ocuda.Models
+{
+    public class CustomerAddress
+    {
+        public string AddressType { get; set; }
+        public int AddressTypeId { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public int CountryId { get; set; }
+        public string County { get; set; }
+        public int Id { get; set; }
+        public string PostalCode { get; set; }
+        public string State { get; set; }
+        public string StreetAddressOne { get; set; }
+        public string StreetAddressTwo { get; set; }
+        public string ZipPlusFour { get; set; }
+    }
+}
